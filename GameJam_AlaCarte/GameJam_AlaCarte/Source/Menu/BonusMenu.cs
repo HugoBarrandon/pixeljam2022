2d201102dc114cf318a25a8596baf67e2b939b7d
Brihmt <73500635+Brihmt@users.noreply.github.com>
2022-03-12 19:39:51 +0100

    sdrfh

--- GameJam_AlaCarte/GameJam_AlaCarte/Source/Menu/BonusMenu.cs
+++ GameJam_AlaCarte/GameJam_AlaCarte/Source/Menu/BonusMenu.cs
@@ -35,16 +35,23 @@ namespace GameJam_AlaCarte.Source.Menu
 
         public void Update(Point position)
         {
-            WindowDimension.X = GraphicsDeviceManager.DefaultBackBufferWidth;
-            WindowDimension.Y = GraphicsDeviceManager.DefaultBackBufferHeight;
+            if (!ChoiceDone)
+            {
+                WindowDimension.X = GraphicsDeviceManager.DefaultBackBufferWidth;
+                WindowDimension.Y = GraphicsDeviceManager.DefaultBackBufferHeight;
+            }
         }
 
 
         public void Draw(SpriteBatch _spriteBatch)
         {
-            _spriteBatch.Draw(TextureFinder.BoatTexture, new Rectangle(0, 0, (int)WindowDimension.X/3, (int)WindowDimension.Y/3), Color.White);
-            _spriteBatch.Draw(TextureFinder.BoatTexture, new Rectangle((int)WindowDimension.X/3, (int)WindowDimension.Y/3, (int)WindowDimension.X / 3, (int)WindowDimension.Y / 3), Color.White);
-            _spriteBatch.Draw(TextureFinder.BoatTexture, new Rectangle((int)WindowDimension.X*2/3, (int)WindowDimension.Y*2/3, (int)WindowDimension.X / 3, (int)WindowDimension.Y / 3), Color.White);
+            if (!ChoiceDone)
+            {   //1600*900
+                _spriteBatch.Draw(TextureFinder.BoatTexture, new Rectangle(0, 0, 100, (int)WindowDimension.Y / 3), Color.White);
+                _spriteBatch.Draw(TextureFinder.BoatTexture, new Rectangle(533,100, 300, 300), Color.White);
+                _spriteBatch.Draw(TextureFinder.BoatTexture, new Rectangle(1066, 100, 300, 300), Color.White);
+
+            }
         }
     }
 }
