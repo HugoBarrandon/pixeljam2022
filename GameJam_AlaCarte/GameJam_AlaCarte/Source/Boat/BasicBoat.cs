06b6288ab1b675b9f3055cc9adf6456a30ca6a12
HolyBlue <hugobarrandon@gmail.com>
2022-03-12 10:20:10 +0100

    Ajout classes

---
+++ GameJam_AlaCarte/GameJam_AlaCarte/Source/Boat/BasicBoat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameJam_AlaCarte.Source.Boat
+{
+    class BasicBoat : Boat
+    {
+
+    }
+}
---
+++ GameJam_AlaCarte/GameJam_AlaCarte/Source/Boat/Boat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameJam_AlaCarte.Source.Boat
+{
+    abstract class Boat
+    {
+        int speed = 1;
+    }
+}
